7857142b1bb7ecb1f0e4e5d3c8ad1214498f36f7
Patrick Pang <imptrik@gmail.com>
2019-12-24 14:59:24 +0800

    feat: sudoku board with colors

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
   
 import Serialization
 import Model
+import Colors
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
@@ -11,8 +12,8 @@ main :: IO ()
 main = do
   (screenWidth, screenHeight) <- getScreenSize
   let 
-    windowWidth = 600
-    windowHeight = 400
+    windowWidth = 360
+    windowHeight = 360
     windowLeft = (screenWidth - windowWidth) `div` 2
     windowTop = (screenHeight - windowHeight) `div` 2
     window = InWindow "Jigsaw Sudoku" (windowWidth, windowHeight) (windowLeft, windowTop)
@@ -21,7 +22,29 @@ main = do
   play window white 100 game renderWorld handleEvent updateWorld
 
 renderWorld :: Game -> Picture
-renderWorld game = Scale 0.1 0.1 $ Text $ show game
+renderWorld game = renderGame game
+
+cellLength :: Float
+cellLength = 40.0
+
+renderGame :: Game -> Picture
+renderGame Game{board=board, config=config} = pictures $ concat 
+  [
+    [
+      translate (((fromIntegral c) - 4) * cellLength) ((4 - (fromIntegral r)) * cellLength) $ 
+        renderCell cell (colorsOfBlocks !! (config !! r !! c))
+      | (c, cell) <- zip [0..] row
+    ] 
+    | (r, row) <- zip [0..] board
+  ]
+
+renderCell :: Cell -> Color -> Picture
+renderCell cell blockColor = pictures 
+  [
+    color blockColor $ rectangleSolid cellLength cellLength,
+    color white $ rectangleWire cellLength cellLength,
+    color black $ translate (- cellLength / 4) (- cellLength / 4) $ scale 0.2 0.2 $ text $ maybe "" show cell
+  ] 
 
 handleEvent :: Event -> Game -> Game
 handleEvent _ game = game
---
+++ src/Colors.hs
@@ -0,0 +1,17 @@
+module Colors where
+
+import  Graphics.Gloss.Data.Color
+
+colorsOfBlocks :: [Color]
+colorsOfBlocks = 
+  [
+    red,
+    green,
+    withAlpha 0.5 blue,
+    yellow,
+    cyan,
+    magenta,
+    rose,
+    chartreuse,
+    azure
+  ]

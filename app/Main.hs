88946cef7f02f7122e88c7d1253215eaa6442bb1
Patrick Pang <imptrik@gmail.com>
2019-12-26 13:19:30 +0800

    feat: keyboard focus navigation
    basic structure confirmed

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,9 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Interface.Environment (getScreenSize)
 
+data State =
+  Playing {game :: Game, focus :: Coord}
+
 main :: IO ()
 main = do
   (screenWidth, screenHeight) <- getScreenSize
@@ -19,35 +22,67 @@ main = do
     window = InWindow "Jigsaw Sudoku" (windowWidth, windowHeight) (windowLeft, windowTop)
 
   game <- loadGame "maps/map.txt"
-  play window white 100 game renderWorld handleEvent updateWorld
+  let state = Playing{game=game, focus=(0,0)}
 
-renderWorld :: Game -> Picture
-renderWorld game = renderGame game
+  play window white 100 state renderWorld handleEvent updateWorld
+
+updateWorld :: Float -> State -> State
+updateWorld _ state = state
+
+renderWorld :: State -> Picture
+renderWorld Playing{game=game, focus=focus} = renderGame game focus
+
 
 cellLength :: Float
 cellLength = 40.0
 
-renderGame :: Game -> Picture
-renderGame Game{board=board, config=config} = pictures $ concat 
+renderGame :: Game -> Coord -> Picture
+renderGame game focus = pictures 
+  [
+    renderBoard game,
+    renderFocus focus
+  ]
+
+renderBoard :: Game -> Picture
+renderBoard Game{board=board, config=config} = pictures $ concat 
   [
     [
       translate (((fromIntegral c) - 4) * cellLength) ((4 - (fromIntegral r)) * cellLength) $ 
-        renderCell cell (colorsOfBlocks !! (config !! r !! c))
+        renderCell cell (config !! r !! c)
       | (c, cell) <- zip [0..] row
     ] 
     | (r, row) <- zip [0..] board
   ]
 
-renderCell :: Cell -> Color -> Picture
-renderCell cell blockColor = pictures 
+renderCell :: Cell -> Int -> Picture
+renderCell cell block = pictures 
   [
-    color blockColor $ rectangleSolid cellLength cellLength,
+    color (colorsOfBlocks !! block) $ rectangleSolid cellLength cellLength,
     color white $ rectangleWire cellLength cellLength,
-    color black $ translate (- cellLength / 4) (- cellLength / 4) $ scale 0.2 0.2 $ text $ maybe "" show cell
-  ] 
+    color black $ translate (- cellLength / 4) (- cellLength / 4) $ 
+      scale 0.2 0.2 $ text $ maybe "" show cell
+  ]
 
-handleEvent :: Event -> Game -> Game
-handleEvent _ game = game
+renderFocus :: Coord -> Picture
+renderFocus (r, c) =
+  color black $ translate (((fromIntegral c) - 4) * cellLength) ((4 - (fromIntegral r)) * cellLength) $
+    rectangleWire cellLength cellLength
 
-updateWorld :: Float -> Game -> Game
-updateWorld _ game = game
+
+handleEvent :: Event -> State -> State
+
+handleEvent (EventKey (SpecialKey KeyUp) Up _ _) state@(Playing{focus=focus}) = state{focus = moveFocus focus (-1) 0}
+handleEvent (EventKey (SpecialKey KeyDown) Up _ _) state@(Playing{focus=focus}) = state{focus = moveFocus focus 1 0}
+handleEvent (EventKey (SpecialKey KeyLeft) Up _ _) state@(Playing{focus=focus}) = state{focus = moveFocus focus 0 (-1)}
+handleEvent (EventKey (SpecialKey KeyRight) Up _ _) state@(Playing{focus=focus}) = state{focus = moveFocus focus 0 1}
+
+handleEvent _ state = state
+
+moveFocus :: Coord -> Int -> Int -> Coord
+moveFocus (r, c) dr dc =
+  let 
+    r' = r + dr
+    c' = c + dc
+  in
+    if 0 <= r' && r' <= 8 && 0 <= c' && c' <= 8 
+    then (r', c') else (r, c)
